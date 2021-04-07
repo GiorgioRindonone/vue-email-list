1043e83fa9d4d6f70f63f6dea5bc80c4dae14079
giorgiorindonone <giorgio.rindonone@gmail.com>
2021-04-07 17:04:25 +0200

    update_start

---
+++ script.js
@@ -0,0 +1,4 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js" integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ==" crossorigin="anonymous"></script>
